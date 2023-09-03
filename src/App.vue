796412d7eca1045310d6ed8cd0cb27c82ca11621
Filip Krunic <fkrunic9@gmail.com>
2023-09-02 23:54:36 -0700

    basic design

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,54 @@
 
 <template>
 <div class="flex items-center justify-center min-h-screen">
-  <h1 className="text-3xl font-bold underline">
-      Hello world!
-  </h1>
+  <!-- Card -->
+  <div class="relative flex flex-col items-center rounded-2xl shadow-xl">
+    <img class="rounded-t-2xl" src="./assets/img/bg-pattern-card.svg" alt="background">
+    <img class="
+      absolute 
+      
+      border-4 
+      border-white 
+      rounded-full
+
+      top-[5.5rem]      
+      " src="./assets/img/image-victor.jpg" alt="victor"
+      >
+    <div class="flex flex-row gap-4 items-center justify-center mt-20">
+      <p class="font-bold text-lg text-very-dark-blue">Victor Crest</p>
+      <p class="text-lg text-dark-gray">26</p>
+    </div>
+    <p class="text-sm text-dark-gray mt-1">London</p>
+
+    <!-- Metrics -->
+    <div class="
+      flex 
+      flex-row 
+      items-center 
+      gap-12 
+      
+      py-6
+      px-12
+      mt-6
+      
+      border-t-[1px]
+      border-solid
+      border-dark-gray/25
+      ">
+      <div class="flex flex-col items-center">
+        <p class="text-xl text-very-dark-blue font-bold">80K</p>
+        <p class="text-xs text-dark-gray tracking-widest">Followers</p>
+      </div>
+      <div class="flex flex-col items-center">
+        <p class="text-xl text-very-dark-blue font-bold">803K</p>
+        <p class="text-xs text-dark-gray tracking-widest">Likes</p>
+      </div>
+      <div class="flex flex-col items-center">
+        <p class="text-xl text-very-dark-blue font-bold">1.4K</p>
+        <p class="text-xs text-dark-gray tracking-widest">Photos</p>
+      </div>      
+    </div>
+  </div>
 </div>
 </template>
 
