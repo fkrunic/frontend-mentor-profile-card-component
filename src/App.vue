25413213002d50159e3b63a8ead04165872063a3
Filip Krunic <fkrunic9@gmail.com>
2023-09-02 23:39:05 -0700

    asset setup

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@
 export default {
   data() {
     return {
-      bodyClass: ''
+      bodyClass: 'font-kumbh-sans'
     };
   },
   mounted() {
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx,vue}",
   ],
   theme: {
+    screens: {
+      desktop: '1440px'
+    },
+    fontFamily: {
+      'kumbh-sans': 'Kumbh Sans, sans-serif'
+    },
+    colors: {
+      'dark-cyan': 'hsl(185, 75%, 39%)',
+      'very-dark-blue': 'hsl(229, 23%, 23%)',
+      'dark-grayish-blue': 'hsl(227, 10%, 46%)',
+      'dark-gray': 'hsl(0, 0%, 59%)',
+      white: 'hsl(0, 0%, 100%)'
+    },
     extend: {},
   },
   plugins: [],
